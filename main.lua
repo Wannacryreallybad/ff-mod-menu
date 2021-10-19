0891f9c3a3ee6ce6f0d1b92b10f2f7c1835b2550
0bBinary <49373598+greasemonkey123@users.noreply.github.com>
2021-10-19 19:43:54 +0300

    welp

--- main.lua
+++ main.lua
@@ -147,7 +147,44 @@ tps_label.Parent = client.PlayerGui:FindFirstChild("GameUI");
 fps_label.Parent = client.PlayerGui:FindFirstChild("GameUI");
 
 local task = task or getrenv().task;
-local fastWait, fastSpawn = task.wait, task.spawn;
+local fastSpawn = task.defer;
+
+runService.Stepped:Connect(function ()
+	local now = tick()
+	local resumePool
+	
+	for thread, resumeTime in pairs(threads) do
+		-- Resume if we're reasonably close enough.
+		local diff = (resumeTime - now)
+		
+		if diff < 0.005 then
+			if not resumePool then
+				resumePool = {}
+			end
+			
+			table.insert(resumePool, thread)
+		end
+	end
+	
+	if resumePool then
+		for _,thread in pairs(resumePool) do
+			threads[thread] = nil
+			coroutine.resume(thread, now)
+		end
+	end
+end)
+
+local function fastWait(t)
+	local t = tonumber(t) or 1 / 3750
+	local start = tick()
+	
+	local thread = coroutine.running()
+	threads[thread] = start + t
+	
+	-- Wait for the thread to resume.
+	local now = coroutine.yield()
+	return now - start, os.clock()
+end
 
 spawn(function()
 	while true do
